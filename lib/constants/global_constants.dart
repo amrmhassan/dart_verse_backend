be3f2d78aca5ba9a292ace07de36c04277f8e256
Amr M Hassan <71602963+amrmhassan@users.noreply.github.com>
2023-08-23 23:59:20 +0300

    buckets path made local

--- lib/constants/global_constants.dart
+++ lib/constants/global_constants.dart
@@ -1,6 +1,6 @@
 class GlobalConst {
   static const String packageName = 'dart_verse';
-  static const String dataDir = '/.$packageName/data';
+  static const String dataDir = './.$packageName/data';
   static const String bucketsDataDir = '$dataDir/buckets';
 }
 
