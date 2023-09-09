c57bf585d25381ff407ead0f0f7ce868fd16fdd7
Amr M Hassan <71602963+amrmhassan@users.noreply.github.com>
2023-09-09 17:41:11 +0300

    crud operations for the

--- lib/dashboard_server/features/app_check/data/datasources/api_key_info_datasource.dart
+++ lib/dashboard_server/features/app_check/data/datasources/api_key_info_datasource.dart
@@ -1,61 +1,30 @@
 import 'package:dart_verse_backend/constants/collections.dart';
 import 'package:dart_verse_backend/dashboard_server/features/app_check/data/datasources/api_key_generator.dart';
+import 'package:dart_verse_backend/dashboard_server/features/app_check/data/datasources/api_keys_repo.dart';
 import 'package:dart_verse_backend/dashboard_server/features/app_check/data/models/api_hash_model.dart';
 import 'package:dart_verse_backend/errors/models/api_key_exceptions.dart';
+import 'package:dart_verse_backend/layers/services/db_manager/data/repositories/mongo_ref/coll_ref_mongo.dart';
 import 'package:dart_verse_backend/layers/services/db_manager/db_service.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
-List<ApiHashModel> _apiKeysHashes = [];
-
-ApiHashModel? _getFromSaved(String apiHash) {
-  ApiHashModel? savedModel = _apiKeysHashes.cast().firstWhere(
-        (element) => element.apiHash == apiHash,
-        orElse: () => null,
-      );
-  return savedModel;
-}
-
-void _addToSavedKeys(ApiHashModel model) {
-  bool exist =
-      _apiKeysHashes.any((element) => element.apiHash == model.apiHash);
-  if (exist) return;
-  _apiKeysHashes.add(model);
-}
-
-void _addAllToSavedKeys(List<ApiHashModel> models) {
-  for (var model in models) {
-    _addToSavedKeys(model);
-  }
-}
-
-void _updateSavedKeys(ApiHashModel model) {
-  int index =
-      _apiKeysHashes.indexWhere((element) => element.apiHash == model.apiHash);
-  if (index == -1) {
-    return;
-  }
-  _apiKeysHashes[index] = model;
-}
-
-void _deleteSavedKey(String hash) {
-  _apiKeysHashes.removeWhere((element) => element.apiHash == hash);
-}
+ApiKeysRepo _apiKeysRepo = ApiKeysRepo();
 
 class ApiKeyInfoDatasource {
   final DbService _dbService;
   final String _encrypterSecretKey;
   late ApiKeyGenerator _generator;
+  late CollRefMongo _collection;
 
   ApiKeyInfoDatasource(this._dbService, this._encrypterSecretKey) {
     _generator = ApiKeyGenerator(encrypterSecretKey: _encrypterSecretKey);
+    _collection =
+        _dbService.mongoDbController.collection(DBCollections.apiKeys);
   }
 
   Future<ApiHashModel?> getApiModel(String apiHash) async {
-    var savedModel = _getFromSaved(apiHash);
+    var savedModel = _apiKeysRepo.getFromSaved(apiHash);
     if (savedModel != null) return savedModel;
-    var doc = await _dbService.mongoDbController
-        .collection(DBCollections.apiKeys)
-        .findOne(where.eq('apiHash', apiHash));
+    var doc = await _collection.findOne(where.eq('apiHash', apiHash));
     if (doc == null) return null;
     var apiModel = ApiHashModel.fromJson(doc);
     return apiModel;
@@ -75,35 +44,27 @@ class ApiKeyInfoDatasource {
   }
 
   Future<void> saveHashModel(ApiHashModel model) async {
-    await _dbService.mongoDbController
-        .collection(DBCollections.apiKeys)
-        .insertOne(model.toJson());
-    _addToSavedKeys(model);
+    await _collection.insertOne(model.toJson());
+    _apiKeysRepo.addToSavedKeys(model);
   }
 
   Future<List<ApiHashModel>> listApiKeys() async {
-    var docs = await _dbService.mongoDbController
-        .collection(DBCollections.apiKeys)
-        .getAllDocuments();
+    var docs = await _collection.getAllDocuments();
     var res = docs.map((e) => ApiHashModel.fromJson(e)).toList();
-    _addAllToSavedKeys(res);
+    _apiKeysRepo.addAllToSavedKeys(res);
     return res;
   }
 
   Future<void> updateApiKey(ApiHashModel newModel) async {
     var selector = where.eq('apiHash', newModel.apiHash);
     var update = modify.set('active', newModel.active);
-    await _dbService.mongoDbController
-        .collection(DBCollections.apiKeys)
-        .updateOne(selector, update);
-    _updateSavedKeys(newModel);
+    await _collection.updateOne(selector, update);
+    _apiKeysRepo.updateSavedKeys(newModel);
   }
 
   Future<void> deleteApiKey(String hash) async {
-    await _dbService.mongoDbController
-        .collection(DBCollections.apiKeys)
-        .deleteOne(where.eq('apiHash', hash));
-    _deleteSavedKey(hash);
+    await _collection.deleteOne(where.eq('apiHash', hash));
+    _apiKeysRepo.deleteSavedKey(hash);
   }
 
   Future<void> toggleApiKeyActiveness(String apiHash) async {
---
+++ lib/dashboard_server/features/app_check/data/datasources/api_keys_repo.dart
@@ -0,0 +1,39 @@
+import 'package:dart_verse_backend/dashboard_server/features/app_check/data/models/api_hash_model.dart';
+
+class ApiKeysRepo {
+  final List<ApiHashModel> _apiKeysHashes = [];
+
+  ApiHashModel? getFromSaved(String apiHash) {
+    ApiHashModel? savedModel = _apiKeysHashes.cast().firstWhere(
+          (element) => element.apiHash == apiHash,
+          orElse: () => null,
+        );
+    return savedModel;
+  }
+
+  void addToSavedKeys(ApiHashModel model) {
+    bool exist =
+        _apiKeysHashes.any((element) => element.apiHash == model.apiHash);
+    if (exist) return;
+    _apiKeysHashes.add(model);
+  }
+
+  void addAllToSavedKeys(List<ApiHashModel> models) {
+    for (var model in models) {
+      addToSavedKeys(model);
+    }
+  }
+
+  void updateSavedKeys(ApiHashModel model) {
+    int index = _apiKeysHashes
+        .indexWhere((element) => element.apiHash == model.apiHash);
+    if (index == -1) {
+      return;
+    }
+    _apiKeysHashes[index] = model;
+  }
+
+  void deleteSavedKey(String hash) {
+    _apiKeysHashes.removeWhere((element) => element.apiHash == hash);
+  }
+}
